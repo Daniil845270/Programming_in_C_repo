23b7ed33b2cf1376c23403cbb297ccdd1d7a29ba
Daniil_Lutskiy <daniillutskiy15@gmail.com>
2024-11-11 18:01:43 +0000

    has done the first exercise

--- week9/Realloc/realloc.c
+++ week9/Realloc/realloc.c
@@ -12,8 +12,6 @@ arr* arr_init(void)
 {
     arr* l = (arr*) ncalloc(1, sizeof(arr));
     l->myarr = (int*) ncalloc(1, sizeof(int) * INITSIZE);
-    l->start = 0; //it is zero anyway, but just to make sure
-    l->end = INITSIZE - 1; // I dont think I'll use it, but lt it be here anyway
     l->size = INITSIZE;
     return l;
 }
@@ -24,22 +22,13 @@ void arr_set(arr *l, int n, int i)
     if (l == NULL){
         on_error("Feeding a function with null pointer");
     }
-    if ((l->size - 1) > n){
-        int minscale = SCALEFACTOR;
-        if ((n / l->size) < SCALEFACTOR){
-            minscale = SCALEFACTOR;
-        }
-        else{
-            minscale = SCALEFACTOR * (1 + (n / l->size)); // i don't really know why I do it like that, but let's see if it works
-        }
-        while ((l->size - 1) > n){
-            l->myarr = (int*) nremalloc(l->myarr, (sizeof(int) * SCALEFACTOR));
-            l->size = l->size * SCALEFACTOR;
-        }
+
+    while ((l->size) < n){
+        l->myarr = (int*) nremalloc(l->myarr, (sizeof(int) * l->size * SCALEFACTOR));
+        l->size = l->size * SCALEFACTOR;
     }
     l->myarr[n] = i;
 }
-
 
 /* Similar to = l[n] */
 int arr_get(arr *l, int n)
@@ -54,12 +43,12 @@ int arr_get(arr *l, int n)
 }
 
 /* Clears all space used, and sets pointer to NULL */
-void arr_free(arr **l) //so l here is a pointer to a pointer that points to my structure
+void arr_free(arr **l)
 {
     if (l == NULL){
         on_error("Feeding a function with null pointer");
     }
-    arr* derefl = *l; //though this pointer I free the array
-    free(derefl->myarr);
-    free(derefl); //and now l should be null
+    free((*l)->myarr);
+    free(*l);
+    *l = NULL;
 }
