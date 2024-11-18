d2d41cbb9efa0a700931b1e0f2834969aa3cfba2
Daniil_Lutskiy <daniillutskiy15@gmail.com>
2024-11-18 14:04:01 +0000

    added a .gitignore file and made non-tested depth.c

--- week10/depth.c
+++ week10/depth.c
@@ -14,21 +14,23 @@ struct node{
 typedef struct node Node;
 
 Node *MakeNode(char c);
-void InsertRandom(Node *t, Node *n);
+void InsertRandom(Node *t, Node *n, int* nd_max, int* nd_itr);
 char *PrintTree(Node *t);
 
 int main(void)
 {
-
    char c;
    Node *head = MakeNode('A');
+   int nd_max = 1;
+   int nd_itr = 1;
    Node *n;
 
    srand(time(NULL));
    for(c = 'B'; c < 'G'; c++){
       n = MakeNode(c);
-      InsertRandom(head, n);
+      InsertRandom(head, n, &nd_max, &nd_itr);
    }
+   printf("max depth is %d\n", nd_max);
    printf("%s\n", PrintTree(head));
    return 0;
 }
@@ -43,23 +45,33 @@ Node *MakeNode(char c)
 
 }
 
-void InsertRandom(Node *t, Node *n)
+void InsertRandom(Node *t, Node *n, int* nd_max, int* nd_itr)
 {
 
    if((rand()%2) == 0){ /* Left */
       if(t->left == NULL){
          t->left = n;
+         if ((*nd_max) < (*nd_itr)){
+            (*nd_max) = (*nd_itr);
+         }
+         (*nd_itr) = 0;
       }
       else{
-         InsertRandom(t->left, n);
+         (*nd_itr)++;
+         InsertRandom(t->left, n, nd_max, nd_itr);
       }
    }
    else{ /* Right */
       if(t->right == NULL){
          t->right = n;
+         if ((*nd_max) < (*nd_itr)){
+            (*nd_max) = (*nd_itr);
+         }
+         (*nd_itr) = 0;
       }
       else{
-         InsertRandom(t->right, n);
+         (*nd_itr)++;
+         InsertRandom(t->right, n, nd_max, nd_itr);
       }
    }
 
