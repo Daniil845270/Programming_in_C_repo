2de77eca51b066033accb3b978b825deab9f4e55
Daniil Lutskiy <daniillutskiy15@gmail.com>
2024-11-24 12:24:59 +0000

    make file was not in the propper format

--- week11/t27.c
+++ week11/t27.c
@@ -1,14 +1,18 @@
 #include "t27.h"
+// #include "driver.c"
 
 // 'Internal' function prototypes 
 // ...
 
 dict* dict_init(void)
 {
+    dict* p;
+    return p;
 }
 
 bool dict_addword(dict* p, const char* wd)
 {
+    return true;
 }
 
 void dict_free(dict** d)
@@ -17,23 +21,29 @@ void dict_free(dict** d)
 
 int dict_wordcount(const dict* p)
 {
+    return 0;
 }
 
 int dict_nodecount(const dict* p)
 {
+    return 0;
 }
 
 dict* dict_spell(const dict* p, const char* str)
 {
+    dict* pp;
+    return pp;
 }
 
 int dict_mostcommon(const dict* p)
 {
+    return 0;
 }
 
 // CHALLENGE1
 unsigned dict_cmp(dict* p1, dict* p2)
 {
+    return 0;
 }
 
 // CHALLENGE2
